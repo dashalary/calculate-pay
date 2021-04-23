0e3492b0295676ac1730e6b38b0be18addb4d3d3
Dasha Lary <dasha4988@gmail.com>
2021-04-23 12:31:27 -0400

    wrote conditionals

--- main.c
+++ main.c
@@ -11,5 +11,51 @@
 
 int main()
 {
+    // declare local variables
+    int hours = 0;
+    double grossPay = 0.0;
+    double taxes = 0.0;
+    double netPay = 0.0;
+
+    printf("Please enter the number of hours worked this week:");
+
+    // get the user input
+    scanf("%d", &hours);
+
+    // calculate the gross pay
+    if (hours <= 40)
+        grossPay = hours * PAYRATE;
+    else
+    {
+        grossPay = 40 * PAYRATE;
+        double overTimePay = (hours - 40) * (PAYRATE * 1.5);
+        grossPay += overTimePay;
+    }
+
+    // calculate the taxes
+    if (grossPay <= 300)
+    {
+        taxes = grossPay * TAXRATE_300;
+    }
+    else if (grossPay > 300 && grossPay <= 450)
+    {
+        taxes = 300 * TAXRATE_300;
+        taxes += (grossPay - 300) * TAXRATE_150;
+    }
+    else if (grossPay > 450)
+    {
+        taxes = 300 * TAXRATE_300;
+        taxes += 150 * TAXRATE_150;
+        taxes += (grossPay - 450) * TAXRATE_REST;
+    }
+
+    // calculate the net pay
+    netPay = grossPay - taxes;
+
+    // display the output
+    printf("Your gross pay this week is: %.2f\n", grossPay);
+    printf("Your taxes this week is %.2f\n", taxes);
+    printf("Your net pay this week is: %.2f\n", netPay);
+
     return 0;
 }
